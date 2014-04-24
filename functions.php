c745487440fb0f40e5d801f57f160800a1adaede
dommuldoon <dom@pointblank.ie>
2014-04-24 15:43:18 +0100

    Update functions.php
    added ajax send mail function for contact template

--- functions.php
+++ functions.php
@@ -283,5 +283,87 @@ global $current_user;
 }
 add_action( 'wp_enqueue_scripts', 'add_cookie_warning' );
 
+/* ajax send mail function for contact template */
+
+add_action( 'wp_ajax_pb_send_mail', 'pb_send_mail_callback' );
+add_action( 'wp_ajax_nopriv_pb_send_mail', 'pb_send_mail_callback');
+
+function pb_send_mail_callback() { 
+   
+   //if($_POST)
+//{
+    $to_Email       = "info@pointblank.ie"; //Replace with recipient email address
+     $user_Subject     = filter_var($_POST["userSubject"], FILTER_SANITIZE_STRING);
+    $subject        =  $user_Subject;
+    
+    
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    
+        //exit script outputting json data
+        $output = json_encode(
+        array(
+            'type'=>'error', 
+            'text' => 'Request must come from Ajax'
+        ));
+        
+        die($output);
+    } 
+    
+    //check $_POST vars are set, exit if any missing
+    if(!isset($_POST["userName"]) || !isset($_POST["userEmail"])  || !isset($_POST["userMessage"]))
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
+        die($output);
+    }
+
+    //Sanitize input data using PHP filter_var().
+    $user_Name        = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
+    $user_Email       = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
+   // $user_Phone       = filter_var($_POST["userPhone"], FILTER_SANITIZE_STRING);
+    $user_Message     = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
+
+    
+    //additional php validation
+    if(strlen($user_Name)<4) // If length is less than 4 it will throw an HTTP error.
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($user_Email, FILTER_VALIDATE_EMAIL)) //email validation
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
+   /* if(!is_numeric($user_Phone)) //check entered data is numbers
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Only numbers allowed in phone field'));
+        die($output);
+    }*/
+    if(strlen($user_Message)<5) //check emtpy message
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        die($output);
+    }
+    
+    //proceed with PHP email.
+    $headers = 'From: '.$user_Email.'' . "\r\n" .
+    'Reply-To: '.$user_Email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+        // send mail
+    $sentMail = @mail($to_Email, $subject, $user_Message .'  -'.$user_Name, $headers);
+    
+    if(!$sentMail)
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_Name .' Thank you for your email'));
+        die($output);
+    }
+//}
+} 
+
 
 ?>
